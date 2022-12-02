062b24ed5090b21b9a6c73ec9ee857fa28d93255
Ismail-1220 <118005408+Ismail-1220@users.noreply.github.com>
2022-12-02 22:23:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
 
-#include "lib.h"
 
-int main() {
 
-    int h;
 
-    std::cin >> h;
 
-    if (numeroprimo(h) == 0) {
 
-        std::cout << "numero primo" << std::endl;
 
-    }
 
-    else {
+    
 
-        std::cout << "numero non primo" << std::endl;
+    
 
-    }
+    
 
-    return 0;
+        
 
-}
+    
+
+    
+
+        
+
+    
+
+  
 
